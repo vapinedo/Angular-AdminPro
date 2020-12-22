3bd25cc59e1bcb12f909e6bf8ffafd615b185564
Victor Pinedo <victorpinedomartinez@gmail.com>
2020-12-22 15:01:03 -0500

    se han creado los componentes grafica1 y progress, junto con un modulo de rutas general

---
+++ src/app/pages/progess/progess.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progess',
+  templateUrl: './progess.component.html',
+  styleUrls: ['./progess.component.css']
+})
+export class ProgessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
